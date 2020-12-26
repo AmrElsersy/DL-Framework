ef54671f00b2d5fd0df7885a4407efde0cd50c3b
tasniimahmed <tasnim_eltaliawie@hotmail.com>
2020-12-26 03:17:59 +0200

    added get_batch for the dataset

--- nn/dataset.py
+++ nn/dataset.py
@@ -4,6 +4,7 @@ class Dataset():
 
     def __init__(self,file):
         images= pd.read_csv(file)
+        global data
         data=images.values
         #features
         self.x= data[:, 1:]
@@ -13,6 +14,13 @@ class Dataset():
 
     def __getitem__(self,index):
         return self.x[index], self.label[index]
+
+    def get_batch(self, batch_size, batch_iterator):
+        #iterating on dataset by batch size=batch_size
+        #everytime  get_batch is called it returns different number of samples
+        self.x= data[batch_iterator*batch_size : batch_size*(batch_iterator+1) , 1:]
+        self.label= data[batch_iterator*batch_size : batch_size*(batch_iterator+1) ,[0]]
+        return self.x, self.label
 
 
 
@@ -26,3 +34,9 @@ features,label= first_data
 
 #label of first sample
 print(label)
+
+#iterating on dataset by batch size=4
+#everytime  get_batch is called it return different four samples
+for i in range(3):
+    print(Datasett.get_batch(4,i))
+    print("####")
