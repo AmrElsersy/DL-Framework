f52412de51767db76ce8fb6cfa018eca9e514519
KhaledAtefMansour <44950887+KhaledAtefMansour@users.noreply.github.com>
2020-12-27 16:45:17 +0200

    Update Linear.py

--- nn/Linear.py
+++ nn/Linear.py
@@ -2,38 +2,38 @@ from abstract_classes import *
 
 class Dense(Layer):
 
-	def __init__(self,indim,outdim):
-		super().__init__()
-		self.init_weights(indim,outdim)
+  def __init__(self,indim,outdim,*args, **kwargs):
+    super().__init__()
+    self.init_weights(indim,outdim)
 
-	def init_weights(self,dim):
-		# xavier weight initialization
-		self.weights['W'] = np.random.randn(indim,outdim) * np.sqrt( 2/(indim+outdim) )
-		self.weights['b'] = np.zeros(1,outdim)
+  def init_weights(self,indim, outdim):
+   # xavier weight initialization
+    self.weights['w'] = np.ones((indim,outdim)) * np.sqrt( 2/(indim+outdim) )
+    self.weights['b'] = np.zeros((1,outdim))
 
-	def forward(self,X):
+  def forward(self,X):
 
-		output = np.dot(X , self.weights['W']) + self.weights['b']
-		self.cache['X'] = X
-		self.cache['output'] = output
+    output = np.dot(X , self.weights['w']) + self.weights['b']
+    self.cache['x'] = X
+    self.cache['output'] = output
 
-		return output
+    return output
 
-	def backward(self,dY):
+  def backward(self,dY):
+    print(self.local_grads)
+    dX = np.dot(dY,self.local_grads['x'].T)
+    dW = np.dot(np.array(self.local_grads['w']).T,dY)
+    db = np.sum(dY, axis = 0, keepdims = True)
+    self.weights_global_grads = {'w': dW, 'b': db}
+    print(self.weights_global_grads)
+    return dX
 
-		dX = np.dot(dY,self.grad['X'].T)
-		X  = self.cache['X']
-		dW = np.dot(self.grad['W'],dY)
-		db = np.sum(dY, axis = 0, keepdims = True)
-		self.weights_global_grads = {'W': dW, 'b': db}
-		return dX
-
-	def calculate_local_grads(self,X):
-
-		grads['X'] = self.weights['W']
-		grads['W'] = X
-		grads['b'] = np.ones_like(self.weight['b'])
-		return grads
+  def calculate_local_grads(self, X):
+    grads = {}
+    grads['x'] = self.weights['w']
+    grads['w'] = X
+    grads['b'] = np.ones_like(self.weights['b'])
+    return grads
 
 
 
