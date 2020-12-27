28572b6ed8eabff77ee25c7e862eb85dd814905f
KhaledAtefMansour <44950887+KhaledAtefMansour@users.noreply.github.com>
2020-12-27 22:31:58 +0200

    Update Linear.py

--- nn/Linear.py
+++ nn/Linear.py
@@ -12,20 +12,18 @@ class Dense(Layer):
     self.weights['b'] = np.zeros((1,outdim))
 
   def forward(self,X):
-
-    output = np.dot(X , self.weights['w']) + self.weights['b']
+    print(self.weights['b'].shape)
+    output = np.dot(self.weights['w'].T ,X) + self.weights['b']
     self.cache['x'] = X
     self.cache['output'] = output
 
     return output
 
   def backward(self,dY):
-    print(self.local_grads)
     dX = np.dot(dY,self.local_grads['x'].T)
     dW = np.dot(np.array(self.local_grads['w']).T,dY)
     db = np.sum(dY, axis = 0, keepdims = True)
     self.weights_global_grads = {'w': dW, 'b': db}
-    print(self.weights_global_grads)
     return dX
 
   def calculate_local_grads(self, X):
