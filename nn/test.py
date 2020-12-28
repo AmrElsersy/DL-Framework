ef8b987b337f613505bbdbf0034d395880298119
AmrElsersy <amrelsersay@gmail.com>
2020-12-28 22:20:54 +0200

    testing

--- nn/test.py
+++ nn/test.py
@@ -1,10 +1,10 @@
 from abstract_classes import *
 from model import Model
 from optim import *
-import matplotlib
-import matplotlib.pyplot as plt
-import matplotlib.animation as animation
-from matplotlib import style
+# import matplotlib
+# import matplotlib.pyplot as plt
+# import matplotlib.animation as animation
+# from matplotlib import style
 from threading import Thread
 import time
 import sys
@@ -49,61 +49,50 @@ class MeanSquareLoss(Function):
     # dL_dYhat = (2 * (Y_hat - Y))
     # m_examples = dL_dYhat.shape[0]
     # return {'x': dL_dYhat / m_examples }
-    return {'x' : 2*(Y_hat - Y).mean(axis=1, keepdims=True)}
+    return {'x' : 2*(Y_hat - Y).mean(axis=1, keepdims=True) }
   def backward(self):
     return self.local_grads["x"]
 
 model = Model()
-# model.add(Dense(3,2))
+model.add(Dense(3,2))
 # model.add(Dense(2,1))
-model.add(Dense(1,1))
 model.set_loss(MeanSquareLoss())
 optim = GradientDecent(model.parameters(),0.001)
 
-x = np.array([1,2,3], dtype=np.float).reshape(1,-1)
-y = np.array([2,4,6], dtype=np.float).reshape(1,-1)
+# x = np.array([1,2,3], dtype=np.float).reshape(1,-1)
+# y = np.array([2,4,6], dtype=np.float).reshape(1,-1)
 
-# x = np.array([ [1, 2, 3],
-#   [4,5,6],
-#   [7,8,9]
-# ],dtype=np.float32).T
-# y = np.array([6, 15, 24], dtype=np.float32).reshape(1,-1)
+x = np.array([ [1, 2, 3],
+  [4,5,6],
+  [7,8,9],
+  [1,0,0]
+],dtype=np.float32).T
+y = np.array([6, 15, 24, 3], dtype=np.float32).reshape(1,-1)
 
-trainingLoss = []
-validationLoss = None
+# print(y.shape)
+# print(y)
 
-def live_graph():
-    """
-    Draw a graph for training and validation loss\n
-    [Required] trainingLoss: Training loss array,\t
-    [optional] validationLoss: Validation loss array,
-    """
-    style.use('fivethirtyeight')
-    fig = plt.figure()
-    ax1 = fig.add_subplot(1, 1, 1)
-    def animate(i):
-        ax1.cla()
-        ax1.plot(np.array(range(len(trainingLoss))) + 1, trainingLoss, label="Training loss")
-        plt.legend(loc='upper right')
-    ani = animation.FuncAnimation(fig, animate, interval=1000)
-    plt.tight_layout()
-    plt.show()
+"""
+[[1. 4. 7. 1.]
+ [2. 5. 8. 0.]
+ [3. 6. 9. 0.]]  .... shape (3, 4)
 
+[[ 6. 15. 24., 3 ]] .... shape  (1, 4)
+"""
 
-Thread(target=live_graph, daemon=True).start()
+# xx = x[:,1].reshape(-1,1)
+# print(xx, xx.shape)
 
-epochs = 10
+# print(x[:, 0].reshape(x.shape[0], 1).shape )
+# print(y[:, 0].reshape(y.shape[0], 1).shape )
+# exit()
+epochs = 1000
 for epoch in range(epochs):
     optim.zero_grad()
-    y_hat = model.forward(x)
-    l = model.loss(y_hat, y)
-    trainingLoss.append(l.squeeze())
+    y_hat = model.forward( x[:, 1].reshape(-1,1) )
+    l = model.loss(y_hat, y[:, 1].reshape(-1,1) )
     model.backward()
     optim.step()
     print("y_hat= ", y_hat, " ... Loss = ", l)
     print("w=",model.layers[0].weights, " ... dw= ", model.layers[0].weights_global_grads["w"]," ... db= ", model.layers[0].weights_global_grads["b"])
     print("==========================================================================================================================================")
-    time.sleep(1)
-
-print(trainingLoss)
-sys.exit(0)
