17b172dc8435a7ffd7371907abb5748520a5d13c
mamoanwar97 <mamoanwar97@gmail.com>
2021-01-21 00:36:32 +0200

    vis

--- MNIST_test.py
+++ MNIST_test.py
@@ -5,15 +5,10 @@ from optim import GradientDecent, MomentumGD,  Adam, StepLR
 from activations import ReLU,Sigmoid
 from loss import CrossEntropyLoss
 from utils import save_weights, load_weights
-from cnn import *
-import time
-import random
-from itertools import count
-import matplotlib.pyplot as plt
-from matplotlib import style
+import matplotlib as plt
 from matplotlib.animation import FuncAnimation
 from threading import Thread
-import sys
+import time
 
 # wieghts path
 path = "ray2_weights.sav"
@@ -49,64 +44,28 @@ model.add(Dense(45, 10))
 model.set_loss(CrossEntropyLoss())
 
 
-optimizer = GradientDecent(model.parameters(), learning_rate = 0.01)
+# optimizer = GradientDecent(model.parameters(), learning_rate = 0.01)
 # optimizer = MomentumGD(model.parameters(), learning_rate = 0.01)
-# optimizer = Adam(model.parameters(), learning_rate = 0.01)
-# lr_schedular = StepLR(optimizer, step_size = 1, gamma=0.1)
+optimizer = Adam(model.parameters(), learning_rate = 0.01)
+lr_schedular = StepLR(optimizer, step_size = 1, gamma=0.1)
 
 # model = load_weights(path)
 
+epochs = 1
+for epoch in range(epochs):
+    i = 0
+    for image, label in dataloader:
+        # if i == 1700:
+        #     break
+        image = image/255
+        i = i + 1
+        print("Iteration no.", i)
+        predicted = model(image)
+        loss = model.loss(predicted, label)
+        model.backward()
+        optimizer.step()
+        # print("loss= ", loss)
+        # time.sleep(0.1)
+        print("===========")
 
 # save_weights(model, path)
-
-trainingLoss = []
-
-def live_graph():
-    """
-    Draw a graph for training and validation loss\n
-    [Required] trainingLoss: Training loss array,\t
-    [optional] validationLoss: Validation loss array,
-    """
-    style.use('fivethirtyeight')
-    fig = plt.figure()
-    ax1 = fig.add_subplot(1, 1, 1)
-    def animate(i):
-        ax1.cla()
-        ax1.plot(np.array(range(len(trainingLoss))) + 1, trainingLoss, label="Training loss")
-        plt.legend(loc='upper right')
-    ani = FuncAnimation(fig, animate, interval=1000)
-    plt.tight_layout()
-    plt.xlabel('number of stations trying to send ')
-    plt.ylabel('Channel efficiency')
-    plt.title('Efficiency of ethernet at 10 Mbps with 512-bit slot times')
-    plt.show()
-
-
-def main():
-    epochs = 10
-    print("start")
-    for epoch in range(epochs):
-        i = 0
-        for image, label in dataloader:
-            # if i == 1700:
-            #     break
-            optimizer.zero_grad()
-            image = image/255
-            i = i + 1
-            print("Iteration no.", i)
-            predicted = model(image)
-            loss = model.loss(predicted, label)
-            trainingLoss.append(loss.squeeze())
-            model.backward()
-            optimizer.step()
-            # print("loss= ", loss)
-            time.sleep(1)
-            print("===========")
-
-t1 = Thread(target=main, daemon=True)
-t1.start()
-live_graph()
-stop_threads = True
-t1.join()
-sys.exit(0)
-
